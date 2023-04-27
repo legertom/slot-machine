841a5cdc5350723ae1fbe5f65d9a9c88bec1a6a2
Tom Leger <tomleger@gmail.com>
2023-04-27 16:12:42 -0400

    first working version

---
+++ README.md
@@ -0,0 +1,70 @@
+# Slot Machine App
+
+A modern and colorful casino-themed slot machine app built with React. Users can deposit coins, adjust their bet, and spin the reels to win payouts based on different combinations.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Features](#features)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Installation
+
+To set up and run the Slot Machine App on your local machine, follow these steps:
+
+1. Clone the repository to your local machine:
+
+```
+git clone https://github.com/legertom/slot-machine.git
+```
+
+2. Navigate to the project directory:
+
+```
+cd slot-machine-app
+```
+
+3. Install the required dependencies:
+
+```
+npm install
+```
+
+4. Start the development server:
+```
+npm start
+```
+
+The app should now be running on `http://localhost:3000`.
+
+## Usage
+
+1. Start by depositing coins using the "Deposit 100 coins" button.
+2. Adjust your bet amount using the input field.
+3. Click the "Spin" button to spin the reels.
+4. If you get a winning combination, you'll receive a payout according to the payout table.
+5. You can view the instructions by clicking the "Instructions" button.
+
+## Features
+
+- Modern and colorful casino-themed user interface
+- Customizable bet amount
+- Deposit and manage coin balance during a session
+- Spin the reels to win payouts based on different combinations
+- Instructions modal for easy access to game rules
+
+## Contributing
+
+Contributions to the Slot Machine App are welcome! To contribute, follow these steps:
+
+1. Fork the repository.
+2. Create a new branch with a descriptive name: `git checkout -b your-feature-name`
+3. Commit your changes: `git commit -m 'Add some feature'`
+4. Push the branch: `git push origin your-feature-name`
+5. Open a pull request with a clear and concise description of your changes.
+
+## License
+
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for details.
---
+++ src/App.js
@@ -0,0 +1,171 @@
+import React, { useState } from "react";
+import "./App.css";
+import cherry from './cherry.png';
+import seven from './seven.png';
+import bar from './bar.png';
+
+
+function App() {
+  // Implementation goes here
+  const [balance, setBalance] = useState(100);
+  const [bet, setBet] = useState(1);
+  const [reels, setReels] = useState(["-", "-", "-"]);
+  const [message, setMessage] = useState("");
+  const reelValues = [seven, bar, cherry];
+  const [hasSpun, setHasSpun] = useState(false);
+
+
+
+
+  function handleDeposit(amount) {
+    // Add the specified amount to the current balance
+    setBalance(balance + amount);
+  }
+  
+  function handleBetChange(event) {
+    // Update the bet amount with the user's input
+    const newBet = parseInt(event.target.value, 10);
+    if (newBet >= 1) {
+      setBet(newBet);
+    } else {
+      setMessage("Bet amount must be at least 1 coin.");
+    }
+  }
+  
+
+  function calculatePayout(reels) {
+    const [reel1, reel2, reel3] = reels;
+  
+    // Check for winning combinations
+    if (reel1 === seven && reel2 === seven && reel3 === seven) {
+      return 500;
+    } else if (reel1 === bar && reel2 === bar && reel3 === bar) {
+      return 250;
+    } else if (reel1 === cherry && reel2 === cherry && reel3 === cherry) {
+      return 100;
+    } else if (
+      (reel1 === cherry && reel2 === cherry) ||
+      (reel1 === cherry && reel3 === cherry) ||
+      (reel2 === cherry && reel3 === cherry)
+    ) {
+      return 50;
+    } else if (reel1 === cherry || reel2 === cherry || reel3 === cherry) {
+      return 10;
+    } else {
+      return 0; // No payout for other combinations
+    }
+  }
+  
+  
+  
+  function handleSpin() {
+    // Generate random values for the reels
+    const newReels = reels.map(() => reelValues[Math.floor(Math.random() * reelValues.length)]);
+    setReels(newReels);
+    console.log(newReels);
+  
+    // Check for winning combination and calculate payout
+    const payout = calculatePayout(newReels);
+  
+    // Check if the user has enough balance for the bet
+    if (balance - bet < 0) {
+      setMessage('You have run out of coins, please deposit more to continue');
+    } else {
+      // Update balance and message
+      setBalance(balance - bet + payout);
+  
+      if (payout > 0) {
+        setMessage(`You won ${payout} coins!`);
+      } else {
+        setMessage('No payout this time. Try again!');
+      }
+    }
+
+    setHasSpun(true);
+  }
+  
+  
+  
+  return (
+    <div className="container">
+    <div className="App">
+      <h1>Slot Machine</h1>
+      <p>
+        Input a coin to start the game. The game spins three reels, each displaying
+        a random value. If all three displayed values are the same, you win a
+        payout. The payout amount is determined by the payout table below. The
+        maximum amount you can win is 500 coins. You can select the amount you
+        want to bet for each spin. You can deposit as many coins as you want in a
+        single session.
+      </p>
+  
+      <div className="balance">
+        <p>Remaining coins: {balance}</p>
+        <button onClick={() => handleDeposit(100)}>Deposit 100 coins</button>
+      </div>
+  
+      <div className="bet">
+        <label htmlFor="betAmount">Bet amount: </label>
+        <input type="number" min="1" id="betAmount" value={bet} onChange={handleBetChange} />
+      </div>
+  
+      <button onClick={handleSpin}>Spin</button>
+  
+      {hasSpun && (
+  <div className="reels">
+    <img src={reels[0]} alt="reel1" className="reel" />
+    <img src={reels[1]} alt="reel2" className="reel" />
+    <img src={reels[2]} alt="reel3" className="reel" />
+  </div>
+)}
+
+
+
+  
+      <h2>Payout Table</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Winning Combination</th>
+            <th>Payout Amount</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Three 7s</td>
+            <td>500 coins</td>
+          </tr>
+          <tr>
+            <td>Three BARs</td>
+            <td>250 coins</td>
+          </tr>
+          <tr>
+            <td>Three cherries</td>
+            <td>100 coins</td>
+          </tr>
+          <tr>
+            <td>Two cherries</td>
+            <td>50 coins</td>
+          </tr>
+          <tr>
+            <td>One cherry</td>
+            <td>10 coins</td>
+          </tr>
+          <tr>
+            <td>Any other combination</td>
+            <td>No payout</td>
+          </tr>
+        </tbody>
+      </table>
+  
+      <div className="message">
+        <p>{message}</p>
+      </div>
+    </div>
+    </div>
+  );
+  
+}
+
+export default App;
+
